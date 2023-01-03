84e5c533e99954f29a2df6f53138ae0d2f87aff2
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-01-02 23:07:17 -0600

    final

--- server.js
+++ server.js
@@ -1,33 +1,74 @@
-const express = require("express")
-const morgan = require("morgan")
+const express = require("express");
+require("colors");
 
-const app = express()
+const app = express();
 
-app.use(morgan('dev'))
+let products = [
+  {
+    id: 1,
+    name: "laptop",
+    price: 1000,
+  },
+];
 
-app.get('/products',(req,res) =>{
-    res.send("Obteniendo productos")
-})
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${req.method.bgGreen} ${req.url.bgYellow}`);
+  next();
+});
 
-app.post('/products',(req,res) =>{
-    res.send("Obteniendo productos")
-})
+app.get("/products", (req, res) => {
+  res.json(products);
+});
 
-app.get('/products',(req,res) =>{
-    res.send("Creando productos")
-})
+app.post("/products", (req, res) => {
+  const { name, price } = req.body;
 
-app.put('/products',(req,res) =>{
-    res.send("Actualizando productos")
-})
+  if (!name || !price) {
+    return res.status(400).send("Product name and price are required");
+  }
 
-app.delete('/products',(req,res) =>{
-    res.send("Eliminado productos")
-})
+  const newProduct = {
+    id: products.length + 1,
+    name,
+    price,
+  };
+  products.push(newProduct);
+  res.status(201).json(newProduct);
+});
 
-app.delete('/products/:id',(req,res) =>{
-    res.send("Un producto")
-})
-app.listen(3000)
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const product = products.find((product) => product.id === parseInt(id));
+  if (!product) {
+    res.status(404).json({ error: "Product not found" });
+  }
+  res.json(product);
+});
 
-console.log('Server on port ${3000}')
+app.delete("/products/:id", (req, res) => {
+  const { id } = req.params;
+  products = products.filter((product) => product.id !== parseInt(id));
+  res.sendStatus(204);
+});
+
+app.all("/products/count", (req, res) => {
+  res.json({ count: products.length });
+});
+
+app.put("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+
+  const product = products.find((product) => product.id === parseInt(id));
+
+  if (!product) return res.status(404).json({ error: "Product not found" });
+
+  product.name = name;
+  product.price = price;
+
+  res.json(product);
+});
+
+app.listen(3000);
+console.log("Server on port 3000");
