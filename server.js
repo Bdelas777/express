5ce4647e20ed6be6f16b6c75f76461142987e21d
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-01-02 22:41:43 -0600

    metodos

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 const express = require("express")
+const morgan = require("morgan")
 
 const app = express()
 
+app.use(morgan('dev'))
+
+app.get('/products',(req,res) =>{
+    res.send("Obteniendo productos")
+})
+
+app.post('/products',(req,res) =>{
+    res.send("Obteniendo productos")
+})
+
+app.get('/products',(req,res) =>{
+    res.send("Creando productos")
+})
+
+app.put('/products',(req,res) =>{
+    res.send("Actualizando productos")
+})
+
+app.delete('/products',(req,res) =>{
+    res.send("Eliminado productos")
+})
+
+app.delete('/products/:id',(req,res) =>{
+    res.send("Un producto")
+})
 app.listen(3000)
 
-console.log("Server on port ${3000}")
+console.log('Server on port ${3000}')
